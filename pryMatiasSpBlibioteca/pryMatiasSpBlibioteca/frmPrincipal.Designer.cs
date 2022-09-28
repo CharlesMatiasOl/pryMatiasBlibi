339ad2cf9e2bd641d5fb326ae8da2a9347ac4c6b
CharlesMatiasOl <114357097+CharlesMatiasOl@users.noreply.github.com>
2022-09-28 13:06:30 -0300

    Add files via upload

---
+++ pryMatiasSpBlibioteca/pryMatiasSpBlibioteca/frmPrincipal.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace pryMatiasSpBlibioteca
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCÓDIGO = new System.Windows.Forms.Label();
+            this.lblNOMBRELIBRO = new System.Windows.Forms.Label();
+            this.lblCÓDIGOEDITORIAL = new System.Windows.Forms.Label();
+            this.lblCÓDIGOAUTOR = new System.Windows.Forms.Label();
+            this.lblCÓDIGODISTRIBUIDOR = new System.Windows.Forms.Label();
+            this.txtNOMBRE = new System.Windows.Forms.TextBox();
+            this.txtCODIGO = new System.Windows.Forms.TextBox();
+            this.txtDISTRIBUIDOR = new System.Windows.Forms.TextBox();
+            this.txtAUTOR = new System.Windows.Forms.TextBox();
+            this.txtEDITORIAL = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCÓDIGO
+            // 
+            this.lblCÓDIGO.AutoSize = true;
+            this.lblCÓDIGO.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCÓDIGO.Location = new System.Drawing.Point(56, 51);
+            this.lblCÓDIGO.Name = "lblCÓDIGO";
+            this.lblCÓDIGO.Size = new System.Drawing.Size(59, 16);
+            this.lblCÓDIGO.TabIndex = 0;
+            this.lblCÓDIGO.Text = "CÓDIGO";
+            // 
+            // lblNOMBRELIBRO
+            // 
+            this.lblNOMBRELIBRO.AutoSize = true;
+            this.lblNOMBRELIBRO.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNOMBRELIBRO.Location = new System.Drawing.Point(56, 85);
+            this.lblNOMBRELIBRO.Name = "lblNOMBRELIBRO";
+            this.lblNOMBRELIBRO.Size = new System.Drawing.Size(108, 16);
+            this.lblNOMBRELIBRO.TabIndex = 1;
+            this.lblNOMBRELIBRO.Text = "NOMBRE LIBRO";
+            // 
+            // lblCÓDIGOEDITORIAL
+            // 
+            this.lblCÓDIGOEDITORIAL.AutoSize = true;
+            this.lblCÓDIGOEDITORIAL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCÓDIGOEDITORIAL.Location = new System.Drawing.Point(56, 120);
+            this.lblCÓDIGOEDITORIAL.Name = "lblCÓDIGOEDITORIAL";
+            this.lblCÓDIGOEDITORIAL.Size = new System.Drawing.Size(132, 16);
+            this.lblCÓDIGOEDITORIAL.TabIndex = 2;
+            this.lblCÓDIGOEDITORIAL.Text = "CÓDIGO EDITORIAL";
+            // 
+            // lblCÓDIGOAUTOR
+            // 
+            this.lblCÓDIGOAUTOR.AutoSize = true;
+            this.lblCÓDIGOAUTOR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCÓDIGOAUTOR.Location = new System.Drawing.Point(56, 159);
+            this.lblCÓDIGOAUTOR.Name = "lblCÓDIGOAUTOR";
+            this.lblCÓDIGOAUTOR.Size = new System.Drawing.Size(110, 16);
+            this.lblCÓDIGOAUTOR.TabIndex = 3;
+            this.lblCÓDIGOAUTOR.Text = "CÓDIGO AUTOR";
+            // 
+            // lblCÓDIGODISTRIBUIDOR
+            // 
+            this.lblCÓDIGODISTRIBUIDOR.AutoSize = true;
+            this.lblCÓDIGODISTRIBUIDOR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCÓDIGODISTRIBUIDOR.Location = new System.Drawing.Point(56, 198);
+            this.lblCÓDIGODISTRIBUIDOR.Name = "lblCÓDIGODISTRIBUIDOR";
+            this.lblCÓDIGODISTRIBUIDOR.Size = new System.Drawing.Size(158, 16);
+            this.lblCÓDIGODISTRIBUIDOR.TabIndex = 4;
+            this.lblCÓDIGODISTRIBUIDOR.Text = "CÓDIGO DISTRIBUIDOR";
+            // 
+            // txtNOMBRE
+            // 
+            this.txtNOMBRE.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNOMBRE.Location = new System.Drawing.Point(252, 82);
+            this.txtNOMBRE.Name = "txtNOMBRE";
+            this.txtNOMBRE.Size = new System.Drawing.Size(164, 22);
+            this.txtNOMBRE.TabIndex = 5;
+            // 
+            // txtCODIGO
+            // 
+            this.txtCODIGO.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCODIGO.Location = new System.Drawing.Point(252, 48);
+            this.txtCODIGO.Name = "txtCODIGO";
+            this.txtCODIGO.Size = new System.Drawing.Size(68, 22);
+            this.txtCODIGO.TabIndex = 6;
+            // 
+            // txtDISTRIBUIDOR
+            // 
+            this.txtDISTRIBUIDOR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDISTRIBUIDOR.Location = new System.Drawing.Point(252, 195);
+            this.txtDISTRIBUIDOR.Name = "txtDISTRIBUIDOR";
+            this.txtDISTRIBUIDOR.Size = new System.Drawing.Size(100, 22);
+            this.txtDISTRIBUIDOR.TabIndex = 7;
+            // 
+            // txtAUTOR
+            // 
+            this.txtAUTOR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAUTOR.Location = new System.Drawing.Point(252, 156);
+            this.txtAUTOR.Name = "txtAUTOR";
+            this.txtAUTOR.Size = new System.Drawing.Size(100, 22);
+            this.txtAUTOR.TabIndex = 8;
+            // 
+            // txtEDITORIAL
+            // 
+            this.txtEDITORIAL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEDITORIAL.Location = new System.Drawing.Point(252, 117);
+            this.txtEDITORIAL.Name = "txtEDITORIAL";
+            this.txtEDITORIAL.Size = new System.Drawing.Size(100, 22);
+            this.txtEDITORIAL.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(203, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(341, 272);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 11;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 370);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtEDITORIAL);
+            this.Controls.Add(this.txtAUTOR);
+            this.Controls.Add(this.txtDISTRIBUIDOR);
+            this.Controls.Add(this.txtCODIGO);
+            this.Controls.Add(this.txtNOMBRE);
+            this.Controls.Add(this.lblCÓDIGODISTRIBUIDOR);
+            this.Controls.Add(this.lblCÓDIGOAUTOR);
+            this.Controls.Add(this.lblCÓDIGOEDITORIAL);
+            this.Controls.Add(this.lblNOMBRELIBRO);
+            this.Controls.Add(this.lblCÓDIGO);
+            this.Name = "frmPrincipal";
+            this.Text = "Blibioteca";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCÓDIGO;
+        private System.Windows.Forms.Label lblNOMBRELIBRO;
+        private System.Windows.Forms.Label lblCÓDIGOEDITORIAL;
+        private System.Windows.Forms.Label lblCÓDIGOAUTOR;
+        private System.Windows.Forms.Label lblCÓDIGODISTRIBUIDOR;
+        private System.Windows.Forms.TextBox txtNOMBRE;
+        private System.Windows.Forms.TextBox txtCODIGO;
+        private System.Windows.Forms.TextBox txtDISTRIBUIDOR;
+        private System.Windows.Forms.TextBox txtAUTOR;
+        private System.Windows.Forms.TextBox txtEDITORIAL;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
